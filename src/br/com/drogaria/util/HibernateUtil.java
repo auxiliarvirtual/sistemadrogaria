de8a8bde044c70ab06c2e7878e89b001d82abc72
Andre Marinho <andre.bmarinho@hotmail.com>
2017-09-19 00:14:03 -0300

    Inserido bibliotecas de projeto anterior

---
+++ src/br/com/drogaria/util/HibernateUtil.java
@@ -0,0 +1,31 @@
+package br.com.drogaria.util;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.AnnotationConfiguration;
+
+public class HibernateUtil {
+	
+private static final SessionFactory	sessionFactory	= buildSessionFactory();
+	
+	public static SessionFactory buildSessionFactory(){
+		
+		try {
+			AnnotationConfiguration cfg = new AnnotationConfiguration();
+			cfg.configure("hibernate.cfg.xml");
+			return cfg.buildSessionFactory();
+			
+		} catch (Exception e) {
+			System.out.println("Criação inicial do objeto SessionFactory falhou. Erro: " + e);
+			throw new ExceptionInInitializerError(e);
+		}
+	}
+	
+	public static SessionFactory getSessionFactory(){
+		return sessionFactory;
+	}
+
+}
