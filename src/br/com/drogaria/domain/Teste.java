ec68822fd9edf353a2996d60e2c19c583fa0fe6b
Andre Marinho <andre.bmarinho@hotmail.com>
2017-10-05 08:33:39 -0300

    Criação da entidade fabricante e configurações no hibernate

---
+++ src/br/com/drogaria/domain/Teste.java
@@ -0,0 +1,12 @@
+package br.com.drogaria.domain;
+
+import br.com.drogaria.util.HibernateUtil;
+
+public class Teste {
+
+	public static void main(String[] args) {
+		HibernateUtil.getSessionFactory();
+
+	}
+
+}
