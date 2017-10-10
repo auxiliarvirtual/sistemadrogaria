59fb5fd4a71131d672bc2cd2fa0edd104587cc1b
Andre Marinho <andre.bmarinho@hotmail.com>
2017-10-09 22:07:52 -0300

    Criação da classe venda e atualização no hibernate.cfg.xml

---
+++ src/br/com/drogaria/domain/Venda.java
@@ -0,0 +1,73 @@
+package br.com.drogaria.domain;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+@Table(name = "tbl_venda")
+public class Venda {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "ven_codigo")
+	private Long codigo;
+
+	@Column(name = "ven_horario", nullable = false)
+	@Temporal(value = TemporalType.TIMESTAMP)
+	private Date horario;
+
+	@Column(name = "ven_total", precision = 7, scale = 2, nullable = false)
+	private BigDecimal valor;
+
+	@ManyToOne(fetch = FetchType.EAGER)
+	@JoinColumn(name = "tbl_funcionarios_fun_codigo", referencedColumnName = "fun_codigo", nullable = false)
+	private Funcionario funcionario;
+
+	
+	public Long getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(Long codigo) {
+		this.codigo = codigo;
+	}
+
+	public Date getHorario() {
+		return horario;
+	}
+
+	public void setHorario(Date horario) {
+		this.horario = horario;
+	}
+
+	public BigDecimal getValor() {
+		return valor;
+	}
+
+	public void setValor(BigDecimal valor) {
+		this.valor = valor;
+	}
+
+	public Funcionario getFuncionario() {
+		return funcionario;
+	}
+
+	public void setFuncionario(Funcionario funcionario) {
+		this.funcionario = funcionario;
+	}
+	
+	
+
+}
